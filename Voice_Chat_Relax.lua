f2808c096daca40108463df2263c1e067b929069
Guest666 <161533853+TOM34322@users.noreply.github.com>
2025-05-05 18:49:15 +0200

    Voice_Chat_Relax.lua

--- Voice_Chat_Relax.lua
+++ Voice_Chat_Relax.lua
@@ -1,24 +1,22 @@
-if game.PlaceId ~= 1137431029 then
-    -- Charge Rayfield juste pour afficher le popup
+if game.PlaceId ~= 102783109733562 then
     local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
     local Window = Rayfield:CreateWindow({
         Name = "Erreur de Jeu",
         LoadingTitle = "Erreur",
-        LoadingSubtitle = "Mauvais Jeu",
+        LoadingSubtitle = "Jeu Incompatible",
         Theme = "Red",
     })
 
     Rayfield:Notify({
-        Title = "❌ Jeu Incompatible",
-        Content = "Ce script fonctionne uniquement dans le jeu ID 1137431029.",
-        Duration = 8,
-        Image = "alert-triangle", -- Icône d'erreur
+        Title = "⛔ Mauvais Jeu",
+        Content = "Ce script ne fonctionne que dans le jeu ID: 1137431029.",
+        Duration = 10,
+        Image = "alert-triangle"
     })
 
-    return -- Stoppe complètement le reste du script
+    return -- Stoppe complètement l'exécution du reste du script
 end
-
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
